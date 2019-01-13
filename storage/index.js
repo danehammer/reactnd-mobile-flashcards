6f6226af4773f0404271fa2a2640635d9297abe9
Dane Hammer <dane.molotok@gmail.com>
2019-01-13 13:48:50 -0600

    add: async storage api

---
+++ storage/index.js
@@ -0,0 +1,40 @@
+import {AsyncStorage} from 'react-native';
+
+const STORAGE_KEY = 'reactnd:mobile-flashcards:decks';
+
+const getDecks = () => {
+  return AsyncStorage.getItem(STORAGE_KEY).then((results) => {
+    return results === null ? {} : JSON.parse(results);
+  });
+};
+
+const getDeck = (id) => {
+  return AsyncStorage.getItem(STORAGE_KEY).then((results) => {
+    const obj = JSON.parse(results);
+    return obj[id];
+  });
+};
+
+const saveDeckTitle = (title) => {
+  return AsyncStorage.mergeItem(
+    STORAGE_KEY,
+    JSON.stringify({
+      [title]: {
+        title: title,
+        questions: []
+      }
+    })
+  );
+};
+
+const addCardToDeck = (title, question) => {
+  return AsyncStorage.getItem(STORAGE_KEY).then((results) => {
+    const obj = JSON.parse(results);
+    const deck = obj[title];
+    deck.questions.push(question);
+    obj[title] = deck;
+    return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(obj));
+  });
+};
+
+export {getDecks, getDeck, saveDeckTitle, addCardToDeck};
